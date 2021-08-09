b6578ac9760f652e40455a9668663b32947467df
mahithachodavarapu <mahitha2799@msitprogram.net>
2021-08-09 11:50:19 +0530

    first commit

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -6,4 +6,10 @@
 
 
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if(m==0):
+		return True
+	elif(n==0):
+		return False
+	else:
+		return m%n==0 
+	# replace with your solution
