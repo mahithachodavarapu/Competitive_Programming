386801e3706a73113177369f72ddeefda4cb985f
mahithachodavarapu <72124216+mahithachodavarapu@users.noreply.github.com>
2021-08-13 08:22:00 +0530

    Update circlesintersect.py

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -5,5 +5,12 @@
 # if the two circles intersect and False otherwise.
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
-	# your code goes here
-	return False 
+	# your code goes here  
+    m = (x1 - x2)**2 + (y1 - y2)**2
+    n = (r1 + r2)**2
+    if (m == n):
+        return True
+    elif (m < n):
+        return True
+    else:
+        return False
