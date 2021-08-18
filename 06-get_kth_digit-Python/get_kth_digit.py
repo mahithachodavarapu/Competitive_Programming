2c0d7209771d36c89515104b03bb1a0ad7decd63
mahithachodavarapu <mahitha2799@msitprogram.net>
2021-08-18 16:00:36 +0530

    final

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -6,4 +6,12 @@
 
 
 def fun_get_kth_digit(digit, k):
-	return 0
+	l=[]
+	if(k>=len(str(digit))):
+    		return 0
+	digit=abs(digit)
+	while(digit):
+		l.append(digit%10)
+		digit=digit//10
+	return l[k]
+#print(fun_get_kth_digit(-789,1))		
