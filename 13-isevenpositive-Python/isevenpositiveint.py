c1a7e672c515de677fc7ac088230f524c96e2975
mahithachodavarapu <mahitha2799@msitprogram.net>
2021-08-18 10:54:08 +0530

    commit

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -6,7 +6,6 @@
 
 def isevenpositiveint(x):
 	# your code goes here
-	if type(x) == int and x>0 and x%2==0:
-		return True
-	else:
- 		return False
+	if(type(x)==int and x>0 and x%2==0):
+    		return True
+	return False
