4da646f80a2ea22e4c2e230ffe1ea5822e52bfef
mahithachodavarapu <mahitha2799@msitprogram.net>
2021-08-18 14:39:45 +0530

    first

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -4,4 +4,29 @@
 
 def mostfrequentdigit(n):
 	# your code goes here
-	pass
+	if(n==0):return 0
+	l=[]
+	while(n):
+		l.append(n%10)
+		n=n//10
+	s=set(l)
+	d={}
+	for i in s:
+		d[i]=l.count(i)
+	
+	
+	return max(d, key=d.get)
+			
+	
+"""
+(24, 2),
+	(0, 0),
+	(26011, 1),
+	(14, 1),
+	(2, 2),
+	(5, 5),
+	(5231123123123, 1),
+	(5312312355565, 5),
+	(1102300, 0)
+"""	
+print(mostfrequentdigit(1102300))
